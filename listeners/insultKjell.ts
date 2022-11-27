eaf9ea158e446048579f42517d25b0ee50bc8670
MaekelbergKjell <maekelbergkjell@gmail.com>
2022-11-27 15:48:48 +0100

    oopsie poopsie

--- listeners/insultKjell.ts
+++
@@ -1,9 +0,0 @@
-import { Client } from "discord.js";
-
-export default (client: Client): void => {
-    client.on('messageCreate', message => {
-        if (message.content === 'kjell?') {
-            message.reply('die jonge is niet normaal mogool');
-        }
-    });
-}; 
