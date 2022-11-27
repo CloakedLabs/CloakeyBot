8c90d15dba31e83bc1e50797fc55a9f3c79f8ae2
MaekelbergKjell <maekelbergkjell@gmail.com>
2022-11-27 16:06:19 +0100

    change insult to includes

--- src/listeners/insultKjell.ts
+++ src/listeners/insultKjell.ts
@@ -2,7 +2,7 @@ import { Client } from "discord.js";
 
 export default (client: Client): void => {
     client.on('messageCreate', message => {
-        if (message.content === 'kjell?') {
+        if (message.content.includes("kjell?")) {
             message.reply('die jonge is niet normaal mogool');
         }
     });
